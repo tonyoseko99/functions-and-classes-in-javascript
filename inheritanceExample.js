7a0bd281986957bd5954fead4f43eb18029ed0dd
imani-prog <timothyimani6@gmail.com>
2025-06-11 16:36:14 +0300

    Add inheritanceExample.js to demonstrate class inheritance

---
+++ inheritanceExample.js
@@ -0,0 +1,22 @@
+class Animal {
+    constructor(name) {
+        this.name = name;
+    }
+
+    speak() {
+        console.log(`${this.name} makes a sound.`);
+    }
+}
+
+class Dog extends Animal {
+    speak() {
+        console.log(`${this.name} barks.`);
+    }
+}
+
+// Example usage:
+const animal = new Animal('Generic Animal');
+animal.speak(); // Generic Animal makes a sound.
+
+const dog = new Dog('Rex');
+dog.speak(); // Rex barks
